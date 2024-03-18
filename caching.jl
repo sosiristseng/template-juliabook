f4eaa60887e209b0839a8c9943f1ad4701620da3
Wen Wei Tseng <sosiristseng@gmail.com>
2024-03-18 16:35:50 +0800

    ready for testing

--- caching.jl
+++ caching.jl
@@ -2,29 +2,24 @@ using SHA
 
 outfile = "nbs.txt"
 basedir = get(ENV, "DOCDIR", "docs") # Defaults to docs/
-cachedir = joinpath(get(ENV, "NBCACHE", ".cache"), basedir) # Defaults to .cache/docs/
-mkpath(cachedir)
-
+cachedir = get(ENV, "NBCACHE", ".cache") # Defaults to .cache/
 nbs = String[]
 
 # Collect the list of notebooks
-# FIXME: why .sha files get overwritten?
 for (root, dirs, files) in walkdir(basedir)
-    # Create respective folders in the cache
-    for dir in dirs
-        mkpath(joinpath(cachedir, dir))
-    end
     for file in files
         if endswith(file, ".ipynb")
             nb = joinpath(root, file)
             shaval = read(nb, String) |> sha256 |> bytes2hex
-            @info "$(nb): SHA256=$(shaval)"
-            shafilename = joinpath(cachedir, file * ".sha")
+            @info "Notebook $(nb): hash=$(shaval)"
+            shafilename = joinpath(cachedir, root, splitext(file)[1] * ".sha")
             # Cache hit
             if isfile(shafilename) && read(shafilename, String) == shaval
                 @info "Notebook $(nb) cache hits and will not be executed."
             # Cache miss
             else
+                @info "Notebook $(nb) cache misses. Writing hash to $(shafilename)."
+                mkpath(dirname(shafilename))
                 write(shafilename, shaval)
                 push!(nbs, nb)
             end
@@ -33,19 +28,20 @@ for (root, dirs, files) in walkdir(basedir)
 end
 
 # Remove cached notebook and sha files if there is no respective notebook
-# for (root, dirs, files) in walkdir(cachedir)
-#     for file in files
-#         if endswith(file, ".ipynb") || endswith(file, ".sha")
-#             nb = joinpath(basedir, file)
-#             if !isfile(nb)
-#                 rm(joinpath(root, file))
-#             end
-#         end
-#     end
-# end
-
-open(outfile, "w") do f
-    for i in nbs
-        println(f, i)
+for (root, dirs, files) in walkdir(cachedir)
+    for file in files
+        if endswith(file, ".ipynb") || endswith(file, ".sha")
+            nb = joinpath(joinpath(splitpath(root)[2:end]), splitext(file)[1] * ".ipynb")
+            if !isfile(nb)
+                fullfn = joinpath(root, file)
+                @info "Notebook $(nb) not found. Removing $(fullfn)."
+                rm(fullfn)
+            end
+        end
     end
+end
+
+# Print the list of notebooks to be executed
+for i in nbs
+    println(i)
 end
